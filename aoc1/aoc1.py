725f1e1e8fe023c58522b0287f2dbbf79c8dc588
Robert Nolet <r.w.nolet@hva.nl>
2024-12-01 10:14:47 +0100

    aoc1

--- aoc1/aoc1.py
+++ aoc1/aoc1.py
@@ -3,7 +3,7 @@
 list1, list2 = zip(*[map(int, line.split()) for line in open('input.txt')])
 
 # Part 1
-print(sum(abs(a-b) for a,b in zip(sorted(list1), sorted(list2))))
+print("Part 1:", sum(abs(a-b) for a,b in zip(sorted(list1), sorted(list2))))
 
 # Part 2
-print(sum(a * list2.count(a) for a in list1))
+print("Part 2:", sum(a * list2.count(a) for a in list1))
